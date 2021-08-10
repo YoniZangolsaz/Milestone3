78bbaf2c01f5faf5230c8aabc604ee9f2b5e21b7
israelfrank <israelfrank123@gmail.com>
2021-08-10 17:36:30 +0300

    update

--- app.ts
+++ app.ts
@@ -1,12 +1,52 @@
-require("dotenv").config();
+import config from './config'
+import * as bodyParser from 'body-parser';
+const env = require("dotenv").config();
 const express = require('express');
 const app = express();
-const port = process.env.SERVER_PORT;
+const port = process.env.SERVER_PORT || config.PORT ;
 
-app.get('/', (req, res) => {
-    res.send('Hey12344')
+app.use(bodyParser.json());
+
+app.post('/api/numbers/prime/validate', (req, res) => {
+    let bodyProperies:number = req.body;
+    let bodyValue = Object.values(bodyProperies);
+    var listOfValue = [];
+
+    bodyValue.forEach(value => {
+        const abc = isPrime(value);
+        listOfValue.push(abc);        
+    });
+    res.send (listOfValue);
+})
+
+app.get('/api/numbers/prime', (req, res) => {
+    let a = (req.query.amount)
+    res.send (a)
+    // let number = (req.query.amount);
+    
 } )
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
+
+function isPrime(num:number):boolean {
+
+    if (num === 2) {
+      return true;
+    } else if (num > 1) {
+      for (var i = 2; i < num; i++) {
+  
+        if (num % i !== 0) {
+          return true;
+        } else if (num === i * i) {
+          return false
+        } else {
+          return false;
+        }
+      }
+    } else {
+      return false;
+    }
+  
+}
---
+++ config.ts
@@ -0,0 +1,6 @@
+
+export default({
+    
+    PORT: 3000,
+    
+});
