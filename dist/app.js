174f44d4223add798832c19ba6669b07ffec4e42
israelfrank <israelfrank123@gmail.com>
2021-08-11 16:43:25 +0300

    update

--- dist/app.js
+++ dist/app.js
@@ -23,34 +23,53 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const config_1 = __importDefault(require("./config"));
-const bodyParser = __importStar(require("body-parser"));
-const env = require("dotenv").config();
 const express_1 = __importDefault(require("express"));
+const bodyParser = __importStar(require("body-parser"));
 const app = express_1.default();
-const port = process.env.SERVER_PORT || config_1.default.PORT;
+const env = require("dotenv").config();
 var isprime = require('isprime');
 app.use(bodyParser.json());
+const port = process.env.SERVER_PORT || config_1.default.PORT;
+app.set('view engine', 'ejs');
 app.post('/api/numbers/prime/validate', (req, res) => {
     let bodyProperies = req.body;
     let bodyValue = Object.values(bodyProperies);
     var listOfValue = [];
-    bodyValue.forEach(value => {
-        let numberResult = isprime(value);
-        listOfValue.push(numberResult);
-    });
-    res.send(listOfValue);
+    for (let i = 0; i < bodyValue.length;) {
+        let numberResult = isprime(bodyValue[i]);
+        if (numberResult === true) {
+            i++;
+        }
+        else {
+            res.send(false);
+        }
+    }
+    res.send(true);
 });
 app.get('/api/numbers/prime/', (req, res) => {
-    var amountValue = (req.query.amount);
-    var listOfNumber = [];
+    let amountValue = (req.query.amount);
+    let listOfNumber = [];
     for (let i = 1; listOfNumber.length < amountValue; i++) {
-        let number = i;
-        let a = isprime(i);
-        if (a === true) {
-            listOfNumber.push(number);
+        let numberToCheck = i;
+        let numerResult = isprime(i);
+        if (numerResult === true) {
+            listOfNumber.push(numberToCheck);
         }
     }
     res.send(listOfNumber);
+});
+app.get('/api/numbers/prime/display', function (req, res) {
+    let listOfNumber = [];
+    for (let i = 1; listOfNumber.length < 10; i++) {
+        let numberToCheck = i;
+        let numerResult = isprime(i);
+        if (numerResult === true) {
+            listOfNumber.push(numberToCheck);
+        }
+    }
+    res.render('index', { number1: listOfNumber[0], number2: listOfNumber[1], number3: listOfNumber[2],
+        number4: listOfNumber[3], number5: listOfNumber[4], number6: listOfNumber[5], number7: listOfNumber[6],
+        number8: listOfNumber[7], number9: listOfNumber[8], number10: listOfNumber[9] });
 });
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
--- src/app.ts
+++ src/app.ts
@@ -1,43 +1,63 @@
-import config from './config'
-import * as bodyParser from 'body-parser';
-const env = require("dotenv").config();
+import config from './config';
 import express from 'express';
+import * as bodyParser from 'body-parser';
 const app = express();
-const port = process.env.SERVER_PORT || config.PORT ;
+const env = require("dotenv").config();
 var isprime = require('isprime');
-
-
 app.use(bodyParser.json());
+
+
+const port = process.env.SERVER_PORT || config.PORT ;
+app.set('view engine', 'ejs');
 
 app.post('/api/numbers/prime/validate', (req, res) => {
     let bodyProperies:number = req.body;
     let bodyValue = Object.values(bodyProperies);
     var listOfValue : boolean[] = [];
-
-    bodyValue.forEach(value => {
-        let numberResult = isprime(value);
-        listOfValue.push(numberResult);        
-    });
-    res.send (listOfValue);
+    
+    for(let i = 0; i < bodyValue.length;) {
+      let numberResult = isprime(bodyValue[i]);
+      if(numberResult === true) {
+        i++;
+      }
+      else {
+        res.send(false)
+      }
+    }
+    // bodyValue.forEach(value => {
+    //     let numberResult = isprime(value);
+    //     listOfValue.push(numberResult);        
+    // });
+    res.send (true);
 })
 
 app.get('/api/numbers/prime/', (req, res) => {
-    var amountValue:any = (req.query.amount);
-    
-    var listOfNumber:number[] = [];
+    let amountValue:any = (req.query.amount); 
+    let listOfNumber:number[] = [];
 
     for (let i = 1; listOfNumber.length < amountValue; i++) {
-      let number = i;
-      let a = isprime(i);
-      if(a === true) {
-        listOfNumber.push(number)
+      let numberToCheck = i;
+      let numerResult = isprime(i);
+      if(numerResult === true) {
+        listOfNumber.push(numberToCheck)
       }
     }
     res.send (listOfNumber)   
 } )
 
-app.get('/api/numbers/prime/display', (req, res) => {
+app.get('/api/numbers/prime/display', function (req, res) {
+  let listOfNumber:number[] = [];
 
+  for (let i = 1; listOfNumber.length < 10; i++) {
+    let numberToCheck = i;
+    let numerResult = isprime(i);
+    if(numerResult === true) {
+      listOfNumber.push(numberToCheck)
+    }
+  }
+  res.render('index', {number1: listOfNumber[0], number2: listOfNumber[1], number3: listOfNumber[2],
+    number4: listOfNumber[3], number5: listOfNumber[4], number6: listOfNumber[5], number7: listOfNumber[6],
+    number8: listOfNumber[7], number9: listOfNumber[8], number10: listOfNumber[9] });
 })
 
 app.listen(port, () => {
